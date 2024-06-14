c57d878990233a29f398e0df37361c64c8e807fe
Mohamed Ahmed <mohamedstarwars@gmail.com>
2024-06-14 18:51:36 +0300

    Adding try-except

--- week1/task2.py
+++ week1/task2.py
@@ -1,7 +1,19 @@
-print("Enter radius of circle: ")
-radius = input()
 
-if radius.isnumeric:
-    radius = float(radius)
-    perimiter = 3.14*radius*radius
-    print("Area: " + str(perimiter))
+def askForRadius():
+    print("Enter radius of circle: ")
+    radius = input()
+    calculatePerimiter(radius)
+
+def calculatePerimiter(radius):
+    if radius.isnumeric:
+        try:
+            radius = float(radius)
+            perimiter = 3.14*radius*radius
+            print("Area: " + str(perimiter))
+        except:
+            askForRadius()
+    else:
+        askForRadius()
+
+if __name__=="__main__":
+    askForRadius()
